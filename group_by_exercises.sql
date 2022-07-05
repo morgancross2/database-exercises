5900b968f261007f3560aa8480bb935f64f986f0
Morgan Cross <morgan.e.cross2@gmail.com>
2022-07-05 16:55:00 -0500

    started, completed through #6

---
+++ group_by_exercises.sql
@@ -0,0 +1,74 @@
+-- 1. Create a new file named group_by_exercises.sql
+USE employees;
+-- 2. In your script, use DISTINCT to find the unique titles in the titles table. How many
+-- unique titles have there ever been? Answer that in a comment in your SQL file.
+SELECT DISTINCT title
+FROM titles;
+-- There are 7 unique titles.
+
+-- 3. Write a query to to find a list of all unique last names of all employees that start 
+-- and end with 'E' using GROUP BY.
+SELECT last_name
+FROM employees
+WHERE last_name LIKE 'e%e'
+GROUP BY last_name;
+
+-- 4. Write a query to to find all unique combinations of first and last names of all 
+-- employees whose last names start and end with 'E'.
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'e%e'
+GROUP BY first_name, last_name;
+
+-- 5. Write a query to find the unique last names with a 'q' but not 'qu'. Include those
+-- names in a comment in your sql code.
+SELECT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+-- Chleq, Lindqvist, and Qiwen
+
+-- 6. Add a COUNT() to your results (the query above) to find the number of employees with
+-- the same last name.
+SELECT last_name, COUNT(last_name)
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+
+-- 7. Find all all employees with first names 'Irena', 'Vidya', or 'Maya'. Use COUNT(*) and
+-- GROUP BY to find the number of employees for each gender with those names.
+
+-- NOT COMPLETE, WILL NOT RUN CORRECTLY
+SELECT first_name, COUNT(*) AS n_same_first_and_gender
+FROM employees
+WHERE first_name LIKE ('Irena','Vidya','Maya')
+GROUP BY first_name;
+
+-- 8. Using your query that generates a username for all of the employees, generate a count
+-- employees for each unique username. Are there any duplicate usernames? BONUS: How many 
+-- duplicate usernames are there?
+
+-- 9. Bonus: More practice with aggregate functions:
+
+-- 9.a Determine the historic average salary for each employee. When you hear, read, or 
+-- think "for each" with regard to SQL, you'll probably be grouping by that exact column.
+
+-- 9.b Using the dept_emp table, count how many current employees work in each department.
+-- The query result should show 9 rows, one for each department and the employee count.
+
+-- 9.c Determine how many different salaries each employee has had. This includes both
+-- historic and current.
+
+-- 9.d Find the maximum salary for each employee.
+
+-- 9.e Find the minimum salary for each employee.
+
+-- 9.f Find the standard deviation of salaries for each employee.
+
+-- 9.g Now find the max salary for each employee where that max salary is greater than
+-- $150,000.
+
+-- 9.h Find the average salary for each employee where that average salary is between $80k
+-- and $90k.

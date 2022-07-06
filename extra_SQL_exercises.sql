04d2ba395f425853ff8039d73773742fb410052c
Morgan Cross <morgan.e.cross2@gmail.com>
2022-07-06 14:08:14 -0500

    Complete through World Database problems, working on bonus problems next.

--- extra_SQL_exercises.sql
+++ extra_SQL_exercises.sql
@@ -250,25 +250,253 @@ WHERE film.title = 'Hunchback Impossible';
 -- unintended consequence, films starting with the letters K and Q have also soared in 
 -- popularity. Use subqueries to display the titles of movies starting with the letters K 
 -- and Q whose language is English.
-
+SELECT f.title, language.name AS language, f.language_id AS lang_id1, language.language_id AS lang_id2
+FROM (
+	SELECT *
+    FROM film
+    WHERE (title LIKE 'K%') OR (title LIKE 'Q%'))
+    AS f
+LEFT JOIN language ON f.language_id = language.language_id
+WHERE language.name = 'English';
 
 -- 14. Use subqueries to display all actors who appear in the film Alone Trip.
-
+SELECT f.title, actor.first_name, actor.last_name
+FROM (
+	SELECT title, film_id
+    FROM film
+    WHERE title = "Alone Trip") as f
+LEFT JOIN film_actor ON f.film_id = film_actor.film_id
+LEFT JOIN actor ON film_actor.actor_id = actor.actor_id;
 
 -- 15. You want to run an email marketing campaign in Canada, for which you will need the 
 -- names and email addresses of all Canadian customers.
-
+SELECT customer.first_name, customer.last_name, customer.email
+FROM customer
+LEFT JOIN address ON customer.address_id = address.address_id
+LEFT JOIN city ON address.city_id = city.city_id
+LEFT JOIN country ON city.country_id = country.country_id
+WHERE country = 'Canada';
 
 -- 16. Sales have been lagging among young families, and you wish to target all family 
 -- movies for a promotion. Identify all movies categorized as famiy films.
-
+SELECT film.title, category.name AS cat_name, film.film_id AS film_id1, film_category.film_id AS film_id2, 
+film_category.category_id AS cat_id1, category.category_id as cat_id2
+FROM film
+LEFT JOIN film_category ON film.film_id = film_category.film_id
+LEFT JOIN category ON film_category.category_id = category.category_id
+LIMIT 10;
 
 -- 17. Write a query to display how much business, in dollars, each store brought in.
-
+SELECT customer.store_id AS Store, SUM(payment.amount)
+FROM customer
+LEFT JOIN payment ON customer.customer_id = payment.customer_id
+GROUP BY Store;
 
 -- 18. Write a query to display for each store its store ID, city, and country.
-
+SELECT store.store_id AS ID, city.city AS city, country.country AS country
+FROM store
+LEFT JOIN address ON store.address_id = address.address_id
+LEFT JOIN city ON address.city_id = city.city_id
+LEFT JOIN country ON city.country_id = country.country_id;
 
 -- 19. List the top five genres in gross revenue in descending order. (Hint: you may need 
 -- to use the following tables: category, film_category, inventory, payment, and rental.)
+SELECT category.name AS genre, SUM(payment.amount) AS gross_revenue
+FROM category
+LEFT JOIN film_category ON category.category_id = film_category.category_id
+LEFT JOIN inventory ON film_category.film_id = inventory.film_id
+LEFT JOIN rental ON inventory.inventory_id = rental.inventory_id
+LEFT JOIN payment ON rental.rental_id = payment.rental_id
+GROUP BY genre
+ORDER BY gross_revenue DESC
+LIMIT 5;
 
+
+-- NEXT SET
+-- 1.a What is the average replacement cost of a film? 
+SELECT AVG(replacement_cost) FROM film;
+
+-- 1.b Does this change depending on the rating of the film?
+SELECT rating, AVG(replacement_cost)
+FROM film
+GROUP BY rating
+ORDER BY rating;
+
+-- 2. How many different films of each genre are in the database?
+SELECT category.name, COUNT(*)
+FROM category
+LEFT JOIN film_category ON category.category_id = film_category.category_id
+LEFT JOIN film ON film_category.film_id = film.film_id
+GROUP BY category.name
+ORDER BY category.name;
+
+-- 3. What are the 5 most frequently rented films?
+SELECT film.title, COUNT(*)
+FROM film
+LEFT JOIN inventory ON film.film_id = inventory.film_id
+LEFT JOIN rental ON inventory.inventory_id = rental.inventory_id
+LEFT JOIN payment ON rental.rental_id = payment.rental_id
+GROUP BY film.title
+ORDER BY COUNT(*) DESC
+LIMIT 5;
+
+-- 4. What are the most profitable films (in terms of gross revenue)?
+SELECT film.title, SUM(payment.amount) AS total
+FROM film
+LEFT JOIN inventory ON film.film_id = inventory.film_id
+LEFT JOIN rental ON inventory.inventory_id = rental.inventory_id
+LEFT JOIN payment ON rental.rental_id = payment.rental_id
+GROUP BY film.title
+ORDER BY total DESC
+LIMIT 5;
+
+-- 5. Who is the best customer?
+SELECT CONCAT(customer.last_name, ', ', customer.first_name) AS name, SUM(payment.amount) AS total
+FROM customer
+LEFT JOIN payment ON customer.customer_id = payment.customer_id
+GROUP BY name
+ORDER BY total DESC
+LIMIT 1;
+
+-- 6. Who are the most popular actors (that have appeared in the most films)?
+SELECT CONCAT(actor.last_name, ', ', actor.first_name) AS actor_name, COUNT(film.film_id) AS total
+FROM actor
+LEFT JOIN film_actor ON actor.actor_id = film_actor.actor_id
+LEFT JOIN film ON film_actor.film_id = film.film_id
+GROUP BY actor_name
+ORDER BY total DESC
+LIMIT 5;
+
+-- 7. What are the sales for each store for each month in 2005?
+SELECT 
+	SUBSTR(payment.payment_date,1,7) AS month, 
+	staff.store_id, 
+	SUM(payment.amount) AS sales
+FROM payment
+LEFT JOIN staff ON payment.staff_id = staff.staff_id
+GROUP BY month, staff.store_id
+HAVING month LIKE '2005%'
+ORDER BY month;
+
+-- 8. Find the film title, customer name, customer phone number, and customer address 
+-- for all the outstanding DVDs
+SELECT 
+	film.title,
+    CONCAT(customer.last_name, ', ', customer.first_name) AS customer_name,
+	address.phone,
+    address.address
+FROM film
+LEFT JOIN inventory ON film.film_id = inventory.film_id
+LEFT JOIN rental ON inventory.inventory_id = rental.inventory_id
+LEFT JOIN customer ON rental.customer_id = customer.customer_id
+LEFT JOIN address ON customer.address_id = address.address_id
+WHERE rental.return_date IS NULL AND rental.rental_date IS NOT NULL;
+    
+    
+-- EMPLOYEES DATABASE
+-- 1. How much do the current managers of each department get paid, relative to the average
+-- salary for the department? Is there any department where the department manager gets paid
+-- less than the average salary?
+USE employees;
+
+-- Below gives avg department salary
+SELECT departments.dept_name, AVG(salaries.salary)
+FROM departments
+LEFT JOIN dept_emp ON departments.dept_no = dept_emp.dept_no
+LEFT JOIN salaries ON dept_emp.emp_no = salaries.emp_no
+WHERE dept_emp.to_date > CURDATE()
+AND salaries.to_date > CURDATE()
+GROUP BY departments.dept_name
+ORDER BY departments.dept_name;
+
+-- Below gives department manager salary
+SELECT departments.dept_name, salaries.salary
+FROM departments
+LEFT JOIN dept_manager ON departments.dept_no = dept_manager.dept_no
+LEFT JOIN salaries ON dept_manager.emp_no = salaries.emp_no
+WHERE dept_manager.to_date > CURDATE()
+AND salaries.to_date > CURDATE()
+ORDER BY departments.dept_name;
+
+/*Tried to do it all in one table. g.salary returned as unable to aggregate g.salary.
+
+SELECT departments.dept_name, g.salary AS manager_salary, AVG(salaries.salary)
+FROM departments
+LEFT JOIN (
+	SELECT salaries.salary, dept_manager.dept_no
+    FROM dept_manager
+    LEFT JOIN salaries ON dept_manager.emp_no = salaries.emp_no
+    WHERE dept_manager.to_date > CURDATE()
+    AND salaries.to_date > CURDATE()
+    ) as g
+	ON departments.dept_no = g.dept_no
+LEFT JOIN dept_emp ON g.dept_no = dept_emp.dept_no
+LEFT JOIN salaries ON dept_emp.emp_no = salaries.emp_no
+WHERE dept_emp.to_date > CURDATE()
+AND salaries.to_date > CURDATE()
+GROUP BY departments.dept_name;
+*/
+
+
+-- WORLD DATABASE
+USE world;
+
+-- 1. What languages are spoken in Santa Monica?
+SELECT countrylanguage.language, countrylanguage.percentage
+FROM city
+LEFT JOIN countrylanguage ON city.countrycode = countrylanguage.countrycode
+WHERE name LIKE "Santa Monica"
+ORDER BY countrylanguage.percentage;
+
+-- 2. How many different countries are in each region?
+SELECT region, count(*) as num_countries
+FROM country
+GROUP BY region
+ORDER BY num_countries;
+
+-- 3. What is the population for each region?
+SELECT region, SUM(population) AS population
+FROM country
+GROUP BY region
+ORDER BY population DESC;
+
+-- 4. What is the popoulation for each continent?
+SELECT continent, SUM(population) AS population
+FROM country
+GROUP BY continent
+ORDER BY population DESC;
+
+-- 5. What is the average life expectancy globally?
+SELECT AVG(LifeExpectancy) FROM country;
+
+-- 6.a What is the average life expectancy for each region? Sort results
+-- from shortest to longest.
+SELECT region, AVG(LifeExpectancy) AS life_expectancy
+FROM country
+GROUP BY region
+ORDER BY life_expectancy;
+
+-- 6.b Each continent?
+SELECT continent, AVG(LifeExpectancy) AS life_expectancy
+FROM country
+GROUP BY continent
+ORDER BY life_expectancy;
+
+-- 7. Find all the countries whose local name is different from the official name.
+SELECT Name, LocalName
+FROM country
+WHERE Name != LocalName;
+
+-- 8. How many countries have a life expectancy less than x?
+
+
+-- 9. What state is city x located in?
+
+
+-- 10. What region of the world is city x located in?
+
+
+-- 11. What country (human readable anme) is city x located in?
+
+
+-- 12. What is the life expectancy in city x?
